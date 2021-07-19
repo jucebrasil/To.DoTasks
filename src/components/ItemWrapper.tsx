e90cb82a98b541b62c3458f6553997851401a43f
Juce Brasil <jucebrasil.ti@gmail.com>
2021-07-19 19:11:51 -0400

    Desafio Tasks

--- src/components/ItemWrapper.tsx
+++ src/components/ItemWrapper.tsx
@@ -1,4 +1,6 @@
-import React, { ReactNode } from 'react';
+
+import React from 'react';
+import { ReactNode } from 'react';
 import { View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
@@ -11,15 +13,15 @@ export function ItemWrapper({ index, children }: ItemWrapperProps) {
   if (index % 2 === 0)
     return (
       <LinearGradient
-        start={{x: 0, y: 0}} 
-        end={{x: 1, y: 0}}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
         colors={['rgba(196, 196, 196, 0.24)', 'rgba(196, 196, 196, 0)']}
         style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}
       >
         {children}
       </LinearGradient>
     )
-  
+
   return (
     <View
       style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,54 @@
-import React, { useState } from 'react';
+
+import React from 'react';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+interface Tasks {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTasks = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldTasks => [...oldTasks, newTasks]);
+
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+
+    const updatedTasks = tasks.map(task => ({ ...task }))
+
+    const foundItem = updatedTasks.find(item => item.id === id);
+
+    if (!foundItem)
+      return;
+
+    foundItem.done = !foundItem.done;//sempre deve retornar false
+    setTasks(updatedTasks);
+
+
+
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const updatedTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(updatedTasks);
   }
 
   return (
@@ -26,18 +57,20 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
+    backgroundColor: '#EBEBEB',
   }
-})
+
+});
